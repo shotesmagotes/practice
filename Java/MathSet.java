587482a9eb4b839fe0a4b7fd9f00e3f9c03194b0
Shota Makino <shota.makino@yahoo.com>
2015-10-14 15:10:28 -0700

    Updated MathSet.java

--- Java/MathSet.java
+++ Java/MathSet.java
@@ -2,12 +2,28 @@ package chapter10Exercises;
 import chapter2Exercises.TextIO;
 import java.util.*;
 
+
+/**
+ * MathSet is a class that provides set operation on user inputted sets of integers. This class is a solution to a problem in 
+ * David Eck's text "Introduction to Java Programming"
+ * @author shota
+ *
+ */
 public class MathSet {
+	/**
+	 * setColl holds the sets as TreeSet<Integer>
+	 * setOperation holds the operators as Characters
+	 * vars is a temporary private variable that's used to process setColl
+	 * result holds the resulting set from the set operations
+	 */
 	private ArrayList<TreeSet<Integer>> setColl;
 	private ArrayList<Character> setOperation;
 	private TreeSet<Integer> vars; 
 	private TreeSet<Integer> result;
 	
+	/**
+	 * lnRead reads data from the command line and parses them into operands and operators, storing them into setColl and setOperation respectively
+	 */
 	public void lnRead(){
 		setColl = new ArrayList<TreeSet<Integer>>();
 		setOperation = new ArrayList<Character>();
@@ -21,8 +37,10 @@ public class MathSet {
 			nextChar = TextIO.getChar();
 			if ( nextChar == '[' ){
 				vars = new TreeSet<Integer>();
-				while (TextIO.peek() != ']') {
-					vals = TextIO.getChar() + "";
+				value = "";
+				
+				vals = TextIO.getChar() + "";				
+				while (!(vals.equals("]"))) {
 					try{
 						Integer.parseInt(vals);
 					} catch (NumberFormatException e){
@@ -34,9 +52,18 @@ public class MathSet {
 							System.out.println("Value of set is not an integer");
 						}
 					}
+					
 					value += vals;
+					vals = TextIO.getChar() + "";
+					
+					if (vals.equals("]")){
+						try {
+							vars.add(Integer.parseInt(value));
+						} catch (NumberFormatException e){
+							System.out.println("Value of set is not an integer");
+						}
+					}
 				} 
-				TextIO.getChar();
 				setColl.add(vars);
 				flag = true;
 			} else if ( ( nextChar == '-' || nextChar == '+' || nextChar == '*' ) && flag ) {
@@ -50,6 +77,10 @@ public class MathSet {
 		result = calculateOutput();
 	}
 	
+	/**
+	 * calculateOutput links the operators parsed from lnRead with the operations they correspond to on sets, in a mathematical sense
+	 * @return
+	 */
 	private TreeSet<Integer> calculateOutput(){
 		if (setColl.size() != (setOperation.size() + 1)){
 			throw new IllegalArgumentException("Number of operands should be 1 less than number of sets");
